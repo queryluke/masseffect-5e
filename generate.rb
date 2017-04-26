a8164e5bc3f07762192bcca3be75a5f5b73a43d5
Luke Aeschleman <lukeaeschleman@gmail.com>
2017-04-25 21:35:22 -0400

    lists for race feats

--- generate.rb
+++ generate.rb
@@ -44,9 +44,21 @@ def config_races(model)
   skills = []
   model['skills'].split('--').each do |a|
     separated = a.split(':')
+    if separated[1] and separated[1].include? ">"
+      listed  = separated[1].split('>').each { |a| a.strip! }
+      text = listed.shift
+      type = 'list'
+      list = listed
+    else
+      text = separated[1] ? separated[1].strip! : ''
+      type = 'paragraph'
+      list = ''
+    end
     skill = {
       'title' => separated[0].strip,
-      'text' => separated[1] ? separated[1].strip : ''
+      'text' => text,
+      'type' => type,
+      'list' => list
     }
     skills << skill
   end
