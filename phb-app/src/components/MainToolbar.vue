cafbc07799af6358e5ea0e5bcd9793b461479f09
Luke Aeschleman <lukeaeschleman@gmail.com>
2017-12-21 15:39:15 -0500

    add google feedback form

--- phb-app/src/components/MainToolbar.vue
+++ phb-app/src/components/MainToolbar.vue
@@ -1,10 +1,16 @@
 <template lang="pug">
   v-toolbar(dark app fixed clipped-left)
-    v-toolbar-side-icon(@click.stop="toggleSidebar")
+    v-toolbar-side-icon(@click.stop="toggleSidebar").hidden-md-and-up
     v-toolbar-title
       router-link(to="/").nav-brand
         img(:src="`../assets/images/me5e.svg`")
         span Mass Effect 5e - Player's Handbook
+    v-spacer
+    v-toolbar-items.hidden-sm-and-down
+      v-btn(href="/masseffect-5e/about/" flat) About
+      v-btn(href="/masseffect-5e/printables/" flat) Printables
+      v-btn(href="/masseffect-5e/news/" flat) News
+      v-btn(href="https://goo.gl/forms/3wZj8QhlsLv3XOJw1" target="_blank" flat) Feedback
 </template>
 
 <script>
@@ -14,7 +20,7 @@
       toggleSidebar () {
         this.$store.commit('toggleSidebar', !this.$store.state.sidebar)
       }
-    }
+    },
   };
 </script>
 
