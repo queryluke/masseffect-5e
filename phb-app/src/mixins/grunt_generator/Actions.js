cf6466ac4627075941d57b475827e9ca945fd83c
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-01-12 18:09:38 -0500

    typo in grunt generator

--- phb-app/src/mixins/grunt_generator/Actions.js
+++ phb-app/src/mixins/grunt_generator/Actions.js
@@ -1,17 +1,7 @@
 export const Actions = {
   methods: {
     setGruntActions(config, grunt) {
-      config.targetDamage = this.getTargetDamage(config, grunt);
       const crMetaLevel = parseFloat(config.cr.cr) <= 1 ? 0 : Math.ceil(parseFloat(config.cr.cr) / 4);
-
-      if (['adept', 'vanguard', 'sentinel'].includes(grunt.sc.id)) {
-        this.setGruntBiotics(config, grunt);
-      }
-      // need to add cantrip for asari, innate spellcasting?
-
-      if (['engineer', 'infiltrator', 'sentinel'].includes(grunt.sc.id)) {
-        this.setGruntTech(config, grunt);
-      }
 
       if (grunt.sc.id === 'infiltrator') {
         grunt.actions.push({
@@ -22,6 +12,7 @@ export const Actions = {
           'It has advantage on Dexterity (Stealth) checks and the first melee, ranged, or tech attack made from Tactical Cloak. ' +
           'When it makes a melee, ranged, or tech attack, tactical cloak ends.'
         });
+        config.effective.ac += 1;
         // need to make this scale better
         const sneakAttackDie = `${Math.ceil(crMetaLevel + 1 + (crMetaLevel / 2))}d6`;
         const extraDmg = this.averageFromDie(sneakAttackDie);
@@ -30,10 +21,20 @@ export const Actions = {
           name: 'Sneak Attack',
           recharge: '1/Turn',
           description: `The infiltrator deals an extra ${extraDmg} (${sneakAttackDie}) damage when it hits a target with a weapon attack and has advantage on the attack roll, ` +
-          'or when the target is within 5 feet of an ally of the infiltrator that is n\'t incapacitated and the assassin doesn\'t have disadvantage on the attack roll.'
+          'or when the target is within 5 feet of an ally of the infiltrator that is n\'t incapacitated and the infiltrator doesn\'t have disadvantage on the attack roll.'
         });
-        config.targetDamage.dmgMin -= extraDmg / 2;
-        config.targetDamage.dmgMax -= extraDmg / 2;
+        config.effective.dmg += extraDmg;
+      }
+
+      config.targetDamage = this.getTargetDamage(config, grunt);
+
+      if (['adept', 'vanguard', 'sentinel'].includes(grunt.sc.id)) {
+        this.setGruntBiotics(config, grunt);
+      }
+      // need to add cantrip for asari, innate spellcasting?
+
+      if (['engineer', 'infiltrator', 'sentinel'].includes(grunt.sc.id)) {
+        this.setGruntTech(config, grunt);
       }
 
       this.setWeaponActions(config, grunt);
