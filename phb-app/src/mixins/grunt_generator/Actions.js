b1bd94b82ab79b7a4678e1ad44bfef19c43d92f5
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-01-12 18:11:49 -0500

    another gg typo

--- phb-app/src/mixins/grunt_generator/Actions.js
+++ phb-app/src/mixins/grunt_generator/Actions.js
@@ -21,7 +21,7 @@ export const Actions = {
           name: 'Sneak Attack',
           recharge: '1/Turn',
           description: `The infiltrator deals an extra ${extraDmg} (${sneakAttackDie}) damage when it hits a target with a weapon attack and has advantage on the attack roll, ` +
-          'or when the target is within 5 feet of an ally of the infiltrator that is n\'t incapacitated and the infiltrator doesn\'t have disadvantage on the attack roll.'
+          'or when the target is within 5 feet of an ally of the infiltrator that isn\'t incapacitated and the infiltrator doesn\'t have disadvantage on the attack roll.'
         });
         config.effective.dmg += extraDmg;
       }
