abe51f0a8c41c0b26c9203b055f0768f2258f9e5
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-02-24 17:41:30 -0500

    fixes #124

--- phb-app/src/components/ClassAttributes.vue
+++ phb-app/src/components/ClassAttributes.vue
@@ -5,8 +5,6 @@
       li #[strong Hit dice:] {{ item.hit_die }} per level
       li #[strong Hit Points at 1st level:] {{ item.hit_points }}
       li #[strong Hit Points at higher levels:] {{ item.increase_hit_points }}
-      li #[strong Shield Points at 1st level:] {{ item.max_shields }}
-      li #[strong Shield Points at higher levels:] {{ item.increase_max_shields }}
     div.hr
     p.headline Proficiencies
     ul.list-unstyled
