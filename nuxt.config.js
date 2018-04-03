1cdb560b3c08720a0d313e93e5629c16402473ca
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-04-02 21:56:08 -0400

    dynamic route cleanup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,14 @@
+// Dynamic Route generation
+const classes = require('./data/classes.json')
+const races = require('./data/races.json')
+const routes = []
+for (const c of classes.data) {
+  routes.push(`/phb/classes/${c.id}`)
+  routes.push(`/print/spell-cards/${c.id}`)
+}
+for (const r of races.data) {
+  routes.push(`/phb/races/${r.id}`)
+}
 
 module.exports = {
   /*
@@ -8,13 +19,16 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'keywords', content: 'mass effect, d&d, dnd, Dungeons & Dragons, 5e, 5th Edition, TRPG, RPG, d20, homebrew, conversion, tabletop'},
+      { name: 'keywords', content: 'mass effect, d&d, dnd, Dungeons & Dragons, 5e, 5th Edition, TRPG, RPG, d20, homebrew, conversion, tabletop' },
       { hid: 'description', name: 'description', content: 'D&D 5th Edition Homebrew featuring the Mass Effect Universe' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
+  },
+  generate: {
+    routes
   },
   plugins: [
     '~/plugins/vuetify.js',
