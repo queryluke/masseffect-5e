e2846941a370e9c5e04a711e0a8c5be0128902d5
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-04-11 16:11:40 -0400

    working on about section

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   v-content
     section
-      v-parallax(src="/images/milky_way.jpg")
+      v-parallax(src="/images/parallax/milky_way.jpg")
         v-layout(column align-center justify-center).white--text
-          img(src="/images/me5e_logo_notm.png" alt="mass effect 5e" height="200")
-          h1.sr-only About Mass Effect 5e
+          img(src="/images/me5e_logo_notm.png" alt="mass effect 5e").mt-5.me-title-logo
+          h1.display-1.mt-5 About #[span.sr-only Mass Effect 5e]
     section
       v-layout(column wrap align-center).my-5
         v-flex(xs12 sm8).my-3
@@ -16,25 +16,34 @@
               v-flex(xs12 md6)
                 v-card.elevation-0.transparent
                   v-card-text.text-xs-center
-                    v-avatar(size="36px")
-                      img(src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460" alt="")
-                  v-card-title(primary-title).layout.justify-center
-                    div.headline.text-xs-center Luke Aeschleman
-                  v-card-text.
-                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                    v-avatar(size="128")
+                      img(src="https://avatars0.githubusercontent.com/u/12414786?s=460&v=4" alt="Luke A")
+                  v-card-title(primary-title).layout.column.justify-center.text-xs-center.pb-0
+                    v-flex.pb-0
+                      div.headline Luke Aeschleman
+                    v-flex.pt-1
+                      div Creator, Site Developer
+                  v-card-text.pt-0
+                    p.
+                      Luke fell in love with the Mass Effect Universe after playing Mass Effect 2. He's since amassed a number
+                      of playthroughs of all the games, DLC, and spent WAY too much time playing ME3 Multiplayer. By
+                      day, Luke is a Web Developer and Librarian at NC State University. By night, he DMs, codes, watches
+                      baseball and practices Si Lum Kung-Fu
               v-flex(xs12 md6)
                 v-card.elevation-0.transparent
                   v-card-text.text-xs-center
-                    v-avatar(size="36px")
-                      img(src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460" alt="")
-                  v-card-title(primary-title).layout.justify-center
-                    div.headline.text-xs-center Hugh Thompson
-                  v-card-text.
-                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                    v-avatar(size="128")
+                      img(src="https://avatars0.githubusercontent.com/u/679172?s=460&v=4" alt="Hugh T")
+                  v-card-title(primary-title).layout.column.justify-center.text-xs-center.pb-0
+                    v-flex.pb-0
+                      div.headline Hugh Thompson
+                    v-flex.pt-1
+                      div Editor-in-Chief
+                  v-card-text.pt-0
+                    p.
+                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
         v-flex(xs12 sm8).my-3
           div.text-xs-center
             h2.headline Contributors
@@ -42,14 +51,28 @@
 </template>
 
 <script>
-export default {
-  head () {
-    return {
-      title: 'Mass Effect 5e | About',
-      meta: [
-        { hid: 'description', name: 'description', content: 'Learn more about the Mass Effect 5e creators and contributors.' }
-      ]
+  import AboutCard from '~/components/misc/AboutCard.vue'
+
+  export default {
+    components: {AboutCard},
+    data () {
+      return {
+        luke: {
+          games: {
+            me1: ['vanguard', 'sentinel'],
+            me2: ['sentinel', 'engineer', 'vanguard', 'adept', 'soldier'],
+            me3: ['sentinel', 'engineer', 'vanguard', 'infiltrator']
+          }
+        }
+      }
+    },
+    head () {
+      return {
+        title: 'Mass Effect 5e | About',
+        meta: [
+          { hid: 'description', name: 'description', content: 'Learn more about the Mass Effect 5e creators and contributors.' }
+        ]
+      }
     }
   }
-}
 </script>
