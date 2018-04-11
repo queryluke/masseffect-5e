2a890104254b0b530ff81c293cf195c0c902f754
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-04-11 11:28:43 -0400

    hype text

--- pages/index.vue
+++ pages/index.vue
@@ -12,19 +12,22 @@
     // Intro Section
     section
       v-container
-        v-layout(column wrap align-center).my-5
+        v-layout(row wrap justify-center).mt-5
           v-flex(xs12 sm10 md8).my-3
             div.text-xs-center
               h3.headline Begin your journey
               p.
-                Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                Mass Effect 5e lets you take the immersive and cooperative story-telling gameplay of Dungeons & Dragons 5th edition
+                and experience new adventures in the Mass Effect universe. Go exploring in the Outer Terminus. Stop a political assassination
+                on Thessia. Battle the Reapers invasion on Earth, Palavan, or Sur'Kesh. Or, solve crimes as a C-SEC officer on the Citadel.
+                With Mass Effect 5e, the adventure is in your hands. You can play anywhere are on the Mass Effect timeline, create
+                new worlds, or revisit old ones. So gear up and start making your own journey.
+        v-layout(column wrap align-center)
           v-flex(xs12)
             v-container(grid-list-xl)
-              v-layout(row wrap align-center)
+              v-layout(row wrap)
                 // Races and classes
-                v-flex(xs12 md4)
+                v-flex(xs12 md4 d-flex)
                   v-card.elevation-0.transparent
                     v-card-text.text-xs-center
                       v-avatar(size="60px")
@@ -35,11 +38,12 @@
                     v-card-title(primary-title).layout.justify-center
                       div.headline.text-xs-center Mix & Match
                     v-card-text.
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                      Mass Effect 5e lets you mix and match a variety of classic alien races with your favorite Mass Effect class.
+                      Wreak havoc on the front lines as a Krogan Shocktrooper. Crush your enemies like a biotic god as a
+                      Volus Commando. With 12 race and 18 subclass options, Mass Effect 5e has a number of options to fit
+                      your play style.
                 // Weapons and armor
-                v-flex(xs12 md4)
+                v-flex(xs12 md4 d-flex)
                   v-card.elevation-0.transparent
                     v-card-text.text-xs-center
                       v-avatar(size="60px" tile).primary
@@ -47,11 +51,11 @@
                     v-card-title(primary-title).layout.justify-center
                       div.headline.text-xs-center Weapons & Armor
                     v-card-text.
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                      Magic is just science we don't understand yet. So we've replaced the plethora of magical items
+                      found in D&D 5th edition with unique weapons and armor. You'll also find a full mod system for upgrading
+                      and customizing your gear.
                 // Enemies
-                v-flex(xs12 md4)
+                v-flex(xs12 md4 d-flex)
                   v-card.elevation-0.transparent
                     v-card-text.text-xs-center
                       v-avatar(size="60px").primary
@@ -59,9 +63,9 @@
                     v-card-title(primary-title).layout.justify-center
                       div.headline.text-xs-center Unique Enemies
                     v-card-text.
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                      Our bestiary is filled with iconic Mass Effect enemies, but custom made using D&D's Dungeon Master Guide
+                      to ensure balanced combat. From Reaper forces to Blue Sun grunts, we've got enough monsters to keep your
+                      players interested and challenged.
     section
       v-parallax(src="/images/parallax/joker.png")
         v-layout(column align-center justify-center).white--text
@@ -75,9 +79,8 @@
               v-card-title(primary-title)
                 div.headline Interactive Handbook
               v-card-text.
-                Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                Our player's handbook is more than a PDF. It's a fully navigable, web app that allows you to browse rules, search for items and spells,
+                and create bookmarks for fequently used information.
               v-card-actions
                 v-btn(to="/phb/intro" flat) Get Started #[v-icon chevron_right]
           v-flex(md5).hidden-sm-and-down.text-xs-center
@@ -90,9 +93,9 @@
               v-card-title(primary-title)
                 div.headline NPC Generator
               v-card-text.
-                Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                Creating NPCs can be a pain, but our Grunt generator is hear to help. With the click of a button, you can create unique
+                NPCs (or 'grunts') that are fully compatible with the monster creation guidelines of the Dungeon Master's Guide. The generator
+                applys the correct Challenge Rating, unqiue class and racial traits, weapons, and spells.
               v-card-actions
                 v-spacer
                 v-btn(to="/tools/grunts" flat) Generate #[v-icon chevron_right]
@@ -102,9 +105,8 @@
               v-card-title(primary-title)
                 div.headline Tabletop Assets
               v-card-text.
-                Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                We've also got a host of tabletop rpg assests. A custom made character sheet and printable spell cards. In addition,
+                we're working on VTT maps and tokens and plenty of other goodies.
               v-card-actions
                 v-btn(to="/assets" flat) Print #[v-icon chevron_right]
           v-flex(md5).hidden-sm-and-down.text-xs-center
@@ -153,6 +155,6 @@
           { hid: 'description', name: 'description', content: 'Ever wanted to play D&D in the Mass Effect Universe? Mass Effect 5e has you covered.' }
         ]
       }
-    },
+    }
   }
 </script>
