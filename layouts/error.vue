f902625a4ce786cba83fe835a1c245890b47f1db
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-04-10 21:02:19 -0400

    error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,10 +1,14 @@
 <template lang="pug">
   v-content
-    v-container(fill-height)
-      v-layout(fill-height).text-xs-center
-        v-flex(xs12)
-          h1(v-if="error.statusCode === 404").display-1 Page Not Found
-          v-btn(to="/").primary Return Home
+    section
+      v-parallax(src="/images/parallax/critical_mission_failure.jpg")
+    section(style="margin-top: -50px;")
+      v-container
+        v-layout(column wrap align-center)
+          v-flex(xs12 sm10 md8)
+            div.text-xs-center
+              h1(v-if="error.statusCode === 404").display-1 Page Not Found
+              v-btn(to="/").primary Resume
 </template>
 
 <script>
