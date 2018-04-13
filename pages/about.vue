3a8bf7e83d8a460269c3581ca29162b6f1ec06e9
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-04-13 10:25:59 -0400

    finished about page

--- pages/about.vue
+++ pages/about.vue
@@ -68,6 +68,63 @@
               If, we've missed anyone it was merely an oversight on our part and not intentional.
               Feel free to contact us and we'll be happy to give you a shout out. Or if you're on the list and want to be
               removed, have your name changed, or update your picture. Contact us as well.
+    section
+      v-layout(column wrap align-center).my-5
+        v-flex(xs12 sm8).my-3
+          div.text-xs-center
+            h2.headline Getting Involved
+            p.
+              We welcome feedback, criticism, questions! Whether you're a playtester with a mile-long list of class balance
+              issues or simply a passer-by with a fun idea, please let us know.
+        v-flex(xs12)
+          v-container(grid-list-xl)
+            v-layout(row wrap)
+              v-flex(xs12 md4 d-flex)
+                v-card.elevation-0.transparent
+                  v-card-text.text-xs-center
+                    v-avatar.primary
+                      v-icon feedback
+                  v-card-title(primary-title).layout.column.justify-center.text-xs-center.pb-0
+                    v-flex.pb-0
+                      div.headline #[a(href="" target="_blank").blue--text.text--lighten-3 Feedback form]
+                    v-flex.pt-1
+                      div Casual
+                  v-card-text.pt-0
+                    p.
+                      Our feedback form can be used for pretty much anything: questions, ideas, clarifications, etc.
+                      It also gives you the option to leave feedback anonymously (but we won't be able to respond). Additionally, we only check the form a few times
+                      a week, so it might be a few days before we get back to you.
+              v-flex(xs12 md4 d-flex)
+                v-card.elevation-0.transparent
+                  v-card-text.text-xs-center
+                    v-avatar.blue
+                      v-icon email
+                  v-card-title(primary-title).layout.column.justify-center.text-xs-center.pb-0
+                    v-flex.pb-0
+                      div.headline #[a(href="" target="_blank").blue--text.text--lighten-3 Email]
+                    v-flex.pt-1
+                      div Normal
+                  v-card-text.pt-0
+                    p.
+                      Alternatively, you can email us directly. We'll be a bit more responsive and can have an ongoing thread of discussion.
+              v-flex(xs12 md4 d-flex)
+                v-card.elevation-0.transparent
+                  v-card-text.text-xs-center
+                    v-avatar
+                      img(src="/images/misc/GitHub-Mark-Light-64px.png" alt="GitHub Logo")
+                  v-card-title(primary-title).layout.column.justify-center.text-xs-center.pb-0
+                    v-flex.pb-0
+                      div.headline GitHub
+                    v-flex.pt-1
+                      div Veteran
+                  v-card-text.pt-0
+                    p.
+                      If you'd like to be involved in ongoing discussions and follow updates more closely, you can jump into the fray
+                      via GitHub. The site has an issues section where we track everything from site bugs to theorycrafting.
+                    ol.pl-3
+                      li #[a(href="https://github.com/join" target="_blank").blue--text.text--lighten-3 Sign Up for a GitHub Account]
+                      li #[a(href="https://github.com/queryluke/masseffect-5e/issues" target="_blank").blue--text.text--lighten-3 Review and comment on our current issues]
+                      li Open new issues
     section.mt-5.pt-5
       v-layout(row wrap justify-center).mt-5
         v-flex(xs12 sm8).my-2
@@ -108,19 +165,24 @@
         },
         contributors: [
           {
-            name: 'SleightxHope',
+            name: 'RJ Haggerty, a.k.a SleightxHope',
             image: 'https://avatars3.githubusercontent.com/u/31451010?s=460&v=4',
-            desc: 'test'
+            desc: 'Before RJ, this system was just a silly thing Hugh and Luke were doing in their spare time. But RJ\'s contributions' +
+            ' really got the ball rolling. The current shape of the classes (subclasses, class features, and progression) are' +
+            ' thanks to RJ, but he\'s also had a hand in weapon and armor mods and play testing'
           },
           {
             name: 'Alex N',
             image: '/images/spells/tech.svg',
-            desc: 'test'
+            desc: 'Alex is a thorough and detail oriented play tester who has pointed out many anomalies and problematic' +
+            ' features. He\'s also been working on a set of VTT maps that we hope to feature on the site soon. He has also had a heavy' +
+            ' hand in the creation of weapon mods.'
           },
           {
-            name: 'TheMarmotKing',
+            name: 'Adam, a.k.a. TheMarmotKing',
             image: 'https://avatars3.githubusercontent.com/u/37369223?s=460&v=4',
-            desc: 'test'
+            desc: 'Adam has provided a lot of invaluable feedback and started many conversations. Most notibally, you can thank' +
+            ' him for the recent boost in class spell acquisition. '
           }
         ]
       }
