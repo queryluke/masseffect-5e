acccd990015a7d2c22bf7939232fe5b82bb393f5
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-04-21 16:21:11 -0400

    refactors drone jockey

--- components/shared/NpcFeatures/NpcTech.vue
+++ components/shared/NpcFeatures/NpcTech.vue
@@ -5,7 +5,7 @@
         #[strong #[em Innate Tech]]. The {{ stats.name }}'s innate tech ability is Intelligence (spell save DC
         {{ stats.techcasting.dc }}, +{{ stats.techcasting.hit }} to hit with tech attacks). It can innately cast the following for
         {{ stats.techcasting.tpSpent }} tech {{ stats.techcasting.tpSpent | pluralize('point') }}:
-      p.my-0 Recharge 4-6: #[em {{ stats.techcasting.spells.map(spell => spell.name).join(', ') }}]
+      p.my-0 {{ stats.techcasting.perDay ? `${stats.techcasting.perDay}/Day` : 'Recharge 4-6' }}: #[em {{ stats.techcasting.spells.map(spell => spell.name).join(', ') }}]
     div(v-else)
       p.mb-1.
         #[strong #[em Tech Powers ({{ stats.techcasting.perDay }}/Day)].] The {{ stats.name }} can cast {{ stats.techcasting.perDay }}
