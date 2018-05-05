343f3a055048725c7a5132f2b202b3a3f312960d
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-05-05 11:06:15 -0400

    fixes #274

--- components/phb/RuleCards/Barriers.vue
+++ components/phb/RuleCards/Barriers.vue
@@ -3,7 +3,7 @@
     span(slot="header") Barriers
     template(slot="text")
       p.
-        Barrier is a cantrip available to Adepts and Vanguards. It generates barrier ticks which are both a casting
+        Barrier is a cantrip available to Adepts, Sentinels, and Vanguards. It generates barrier ticks which are both a casting
         resource and damage reduction mechanic. Barrier ticks start at 0 and can be gained by casting Barrier or other abilities.
         You cannot have more barrier ticks than the amount indicated on your class progression table.
       p.title.mt-2 Using Barrier Ticks
