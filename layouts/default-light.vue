29858c7b391a988df8e378a53df6be9c340df428
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-06-27 15:49:43 -0400

    create light version of dark layout

--- layouts/default-light.vue
+++ layouts/default-light.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
   v-app(toolbar)
     main-toolbar
+    side-navigation(v-if="$vuetify.breakpoint.mdAndDown")
     nuxt
     main-footer
 </template>
 
 <script>
-import MainToolbar from '~/components/MainToolbar.vue'
-import MainFooter from '~/components/MainFooter.vue'
+  import MainToolbar from '~/components/MainToolbar.vue'
+  import MainFooter from '~/components/MainFooter.vue'
+  import SideNavigation from '~/components/SideNavigation.vue'
 
-export default {
-  components: {
-    MainFooter,
-    MainToolbar
+  export default {
+    components: {
+      MainFooter,
+      MainToolbar,
+      SideNavigation
+    }
   }
-}
 </script>
