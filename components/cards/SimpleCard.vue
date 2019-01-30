fc140a5613a49aa924ec98198a14cf183bdfba0e
Luke A <lukeaeschleman@gmail.com>
2019-01-30 15:49:31 -0500

    Add Unit Test Utils (#397)
    * add unit tests
    * tests ensure grunt generator works
    * fixed grunt generator

---
+++ components/cards/SimpleCard.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  v-card
+    v-card-text
+      slot(name="text")
+</template>
+
+<script>
+  export default {
+    name: 'SimpleCard'
+  }
+</script>
+
--- plugins/globals.js
+++ plugins/globals.js
@@ -3,8 +3,23 @@ import MarkdownFile from '~/components/MarkdownFile.vue'
 import MarkdownContent from '~/components/MarkdownContent.vue'
 import GalaxyMap from '~/components/rule_partials/GalaxyMap.vue'
 import MeHeaderLogo from '~/components/MeHeaderLogo'
+import SimpleCard from '~/components/cards/SimpleCard'
+
+// Because Vue-loader dynamically adds vue components as needed
+// Any vue components used in the .md files need to be loaded globally
+// TODO: Reduce these where possible
+import VChip from 'vuetify/lib/components/VChip'
+import VAlert from 'vuetify/lib/components/VAlert'
+import VBtn from 'vuetify/lib/components/VBtn'
+import VIcon from 'vuetify/lib/components/VIcon'
 
 Vue.component('markdown-file', MarkdownFile)
 Vue.component('markdown-content', MarkdownContent)
 Vue.component('galaxy-map', GalaxyMap)
 Vue.component('me-header-logo', MeHeaderLogo)
+Vue.component('v-chip', VChip)
+Vue.component('v-alert', VAlert)
+Vue.component('v-btn', VBtn)
+Vue.component('v-icon', VIcon)
+Vue.component('simple-card', SimpleCard)
+
