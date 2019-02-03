4c192c56ff2fbdf2308f74b446c4e144ac7e0b12
Luke Aeschleman <lukeaeschleman@gmail.com>
2019-02-03 12:03:55 -0500

    removed unused feat component

--- components/race/RaceInfo.vue
+++ components/race/RaceInfo.vue
@@ -38,14 +38,6 @@
           span(slot="header") Speed
           span(slot="text") {{ race.speed }}
         racial-trait(v-for="trait in race.traits" v-bind:key="trait" v-bind:id="trait")
-        trait(v-if="race.startingFeats")
-          span(slot="header") {{ race.startingFeats.count | pluralize('Feat') }}
-          template(slot="text")
-            span {{ race.startingFeats.count === 1 ? 'Choose one feat:' : 'Choose two feats:' }}
-            span.comma-list
-              span(v-for="(feat, index) in race.startingFeats.options" v-bind:key="feat")
-                feat(v-bind:id="feat").ml-1
-                span(v-if="index + 1 < race.startingFeats.options.length") ,
         trait
           span(slot="header") Optional Starting Credits
           span(slot="text") {{ race.startingCredits }}
@@ -61,11 +53,10 @@
   import Increases from '~/components/race/Increases.vue'
   import Trait from '~/components/race/Trait.vue'
   import RacialTrait from '~/components/race/RacialTrait.vue'
-  import Feat from '~/components/Feat.vue'
   import Variant from '~/components/race/Variant.vue'
 
   export default {
-    components: { MeIcon, Increases, Trait, RacialTrait, Feat, Variant },
+    components: { MeIcon, Increases, Trait, RacialTrait, Variant },
     props: {
       race: {
         type: Object,
