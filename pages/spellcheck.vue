07ad8360cfab88da5a960237c7b2e8221641b108
Luke Aeschleman <lukeaeschleman@gmail.com>
2019-03-17 17:27:18 -0400

    armor sets spell checked

--- pages/spellcheck.vue
+++ pages/spellcheck.vue
@@ -2,21 +2,31 @@
   v-content
 
     // Armor Mods
-    div(v-for="am in armorMods" v-bind:key="am.id")
+    // div(v-for="am in armorMods" v-bind:key="am.id")
       p.title {{ am.name }}
       p {{ am.description }}
       p {{ am.feature }}
+
+    // Armor Sets
+    div(v-for="item in armorSets" v-bind:key="item.id")
+      p.title {{ item.name }}
+      p {{ item.description }}
+      p {{ item.feature }}
+      p {{ item.setBonus }}
+
 
 
 </template>
 
 <script>
   import armorMods from '~/static/data/armor_mods.json'
+  import armorSets from '~/static/data/armor_sets.json'
 
   export default {
     data () {
       return {
-        armorMods: armorMods
+        armorMods,
+        armorSets
       }
     },
     head () {
