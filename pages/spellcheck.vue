1b6cf6e2043037f053f783df30a1386d0b62a81e
Luke Aeschleman <lukeaeschleman@gmail.com>
2019-03-17 16:58:03 -0400

    Spell check armor mods

---
+++ pages/spellcheck.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  v-content
+
+    // Armor Mods
+    div(v-for="am in armorMods" v-bind:key="am.id")
+      p.title {{ am.name }}
+      p {{ am.description }}
+      p {{ am.feature }}
+
+
+</template>
+
+<script>
+  import armorMods from '~/static/data/armor_mods.json'
+
+  export default {
+    data () {
+      return {
+        armorMods: armorMods
+      }
+    },
+    head () {
+      return {
+        title: `Mass Effect 5e`
+      }
+    },
+    layout: 'print'
+  }
+</script>
