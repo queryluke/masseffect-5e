3d43c74cc88c662e43a8bf58548cfe324bf79773
Luke A <lukeaeschleman@gmail.com>
2020-06-02 21:04:38 -0400

    V120 ext (#413)
    * Character Builder BETA (initial build) (#1) (#412)
    
    * Added Character Sheet Vue
    
    * Added Placeholder Skills and Stats tables
    
    * Updates to Stats table formatting
    
    * Added profeciency and styling to skills table
    
    * added races, classes, subclasses
    
    * added dynamic ability scores
    
    * fixed dynamic ability score logic to use watch
    
    * Working on profeciency checkbox bug
    
    * fixed checkbox bug and generalized stats table
    
    * update skills to be dynamically populated
    
    * added image url picker
    
    * Added saving and loading
    
    * character sheet, new post, adding discord and shared assets
    
    Co-authored-by: bdc4 <17170670+bdc4@users.noreply.github.com>

--- pages/assets.vue
+++ pages/assets.vue
@@ -7,67 +7,28 @@
 
       hr.ma-3
 
-      // Character Sheets
-      h2.display-1.font-weight-thin Character Sheets
-      h3.title.mt-3 Printable
-
-      h4 Version 2
-      ul
-        li
-          a(href="https://drive.google.com/open?id=1wN7ALQLWtKoBmqOJtW10H7KpsYkjCM_A" target="_blank") Generic
-      p created by Chris "OodSigma13" Dickson
-
-      h4 Version 1
-      ul
-        li
-          a(href="https://drive.google.com/open?id=1aBCbmwDgmChg6d6K0yegHTab-R6cQD28" target="_blank") Official
-        li
-          a(href="https://drive.google.com/open?id=1GLotuedjZ7AtLMhooBmhEFSU8TRuI4A7" target="_blank") Fillable by /u/rpk0714
-
-      h3.title.mt-3 Google Sheets
-      ul
-        li
-          a(href="https://drive.google.com/open?id=1mj7JDIerstCcQjowNsfasNr42CcKCFrOIf3RPYRC1DA" target="_blank") Official
-        li
-          a(href="https://drive.google.com/open?id=1NesskTT0bxRF2D4ImuyWUo1Mu389Q-KP3uo934mQF-w" target="_blank") by Ben McPherson
-        li
-          a(href="https://drive.google.com/open?id=1r-JCa_rqmKSp7-UKf1EP67w6Dl_Pm_JEdaZJMS-R22Y" target="_blank") by GameMasterPo
-
-      // Roll20
-      h3.title.mt-3 Roll20
+      h2.display-1.font-weight-thin Character Sheets, Roll20 Assets, and other things
       p.
-        An official Roll20 Character Sheet has proved to be a very difficult task. There have been a number of attempts, which are
-        linked below. Mileage may vary. If you do get a version working, please let us know.
+        The discord community has been busy collecting a lot of great assets. To make deliver of that content faster,
+        everything can be found in a public Google Drive.
+
       ul
         li
-          a(href="https://www.reddit.com/r/masseffect5e/comments/cqwokr/updated_roll20_5e_ogl_sheet/" target="_blank") by u/Kiva126
+          a(href="https://drive.google.com/drive/u/0/folders/10S3R_Tfi4bJ9X2b6-4OI65m9F8fx6i0P" target="_blank") All N7.World Assets
         li
-          a(href="https://www.reddit.com/r/masseffect5e/comments/c7h01l/updated_roll20_sheet/" target="_blank") by /u/Danrconway
+          a(href="https://drive.google.com/drive/folders/1RsYfHzmvv-md8_dIrQU65LpWzTr0kpCR?usp=sharing" target="_blank") Character Sheets
+        li
+          a(href="https://drive.google.com/drive/folders/1IbS1Rgym6vUjA9ntYgYMmit7dzhANoaI?usp=sharing" target="_blank") Roll20 Sheets & Tokens
+        li
+          a(href="https://drive.google.com/drive/folders/1MJCLI665gTVni_swC2tMUd1cv8GILF0u?usp=sharing" target="_blank") Premade Characters
+        li
+          a(href="https://drive.google.com/drive/folders/1WpR2M9CpNKKRYTS_Gzi3Woq8NscRkxOY?usp=sharing" target="_blank") Other items (spell card box, equipment sheets, etc.)
 
       hr.ma-3
 
       // Spell Cards
       h2.display-1.font-weight-thin Spell Cards
       v-btn(to="/print/spell-cards") View
-
-      hr.ma-3
-
-      // Premade
-      h2.display-1.font-weight-thin Premade Characters
-      p If you'd like to contribute more pregens see #[a(href="https://www.reddit.com/r/masseffect5e/comments/ch190c/pregens/" target="_blank") this post].
-      v-btn(href="https://drive.google.com/open?id=1XlaCFmUpouNIaQSa1w8SnFRNEQrDcfpX" target="_blank") View
-
-      hr.ma-3
-
-      h2.display-1.font-weight-thin Other
-      ul
-        li
-          a(href="https://drive.google.com/open?id=1SfFEb6jgh2J2SXIiWVUB5XtrSSQ3GEi8" target="_blank") Roll20 Tokens by Andrew Ouellette
-        li
-          a(href="https://drive.google.com/file/d/0BxLeDaHVwlDzcjZtRFFjWUFQb0dGS0NEcV9nQm8xbkw5d0tV/view?usp=sharing" target="_blank") Printable Tabletop Tokens by B. Coar
-        li
-          a(href="https://drive.google.com/open?id=0BxLeDaHVwlDzczdTZlBNOERsR2Z4cVN2WVI2dG9LYlBFbFlv" target="_blank") Spell Card Box by B. Coar
-
 
 </template>
 
