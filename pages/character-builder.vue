378819472c79a3c84ae4e1fa3db20b09ccc833cf
Luke Aeschleman <lukeaeschleman@gmail.com>
2020-06-10 07:16:56 -0400

    resolve merge cf

--- pages/character-builder.vue
+++ pages/character-builder.vue
@@ -5,13 +5,7 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-import CharacterBuilder from "~/components/CharacterSheet.vue";
-=======
-
 import CharacterBuilder from "~/components/character_builder/CharacterSheet.vue"
-
->>>>>>> pr/414
 export default {
   components: { CharacterBuilder }
 };
