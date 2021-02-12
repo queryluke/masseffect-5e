a24eb8c10c4bf0d2884b88fdb3d5cae24a13f069
Luke Aeschleman <lukeaeschleman@gmail.com>
2021-02-12 14:03:24 -0500

    fix anchor links

--- nuxt.config.js
+++ nuxt.config.js
@@ -107,6 +107,41 @@ export default {
     }
   },
   /*
+  ** Router Scroll behavior
+  */
+  router: {
+    scrollBehavior: async (to, from, savedPosition) => {
+      if (savedPosition) {
+        return savedPosition
+      }
+
+      const findEl = async (hash, x = 0) => {
+        return (
+          document.querySelector(hash) ||
+          await new Promise((resolve) => {
+            if (x > 50) {
+              return resolve(document.querySelector('#app'))
+            }
+            setTimeout(() => {
+              resolve(findEl(hash, ++x || 1))
+            }, 100)
+          })
+        )
+      }
+
+      if (to.hash) {
+        const el = await findEl(to.hash)
+        if ('scrollBehavior' in document.documentElement.style) {
+          return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
+        } else {
+          return window.scrollTo(0, el.offsetTop)
+        }
+      }
+
+      return { x: 0, y: 0 }
+    }
+  },
+  /*
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
   */
