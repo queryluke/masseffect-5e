641a243d5c083164fc0a90ce88156bb40694bad6
Luke Aeschleman <lukeaeschleman@gmail.com>
2021-02-24 15:08:46 -0500

    update error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,10 @@
-<template lang="pug">
-  div
-    section
-      v-parallax(src="/images/parallax/critical_mission_failure.jpg")
-    section(style="margin-top: -50px;")
-      v-container
-        v-layout(column wrap align-center)
-          v-flex(xs12 sm10 md8)
-            div.text-xs-center
-              h1(v-if="error.statusCode === 404").display-1 Page Not Found
-              v-btn(to="/").primary Resume
+<template>
+  <v-container>
+    <v-parallax src="/images/parallax/critical_mission_failure.jpg" />
+    <v-btn to="/manual/intro" nuxt>
+      Resume
+    </v-btn>
+  </v-container>
 </template>
 
 <script>
@@ -19,7 +15,6 @@ export default {
       type: Object,
       default: () => { return {} }
     }
-  },
-  layout: 'info'
+  }
 }
 </script>
