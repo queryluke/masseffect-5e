7dfc789cfa0cb864e674db21389a30b82befab90
Luke Aeschleman <lukeaeschleman@gmail.com>
2021-03-02 22:05:05 -0500

    fix gmg, rules

--- nuxt.config.js
+++ nuxt.config.js
@@ -138,19 +138,23 @@ export default {
       const endpoints = ['conditions', 'tool-profs', 'armor', 'backgrounds', 'bestiary', 'changelog', 'classes', 'feats', 'gmg', 'rules', 'powers', 'species', 'weapons']
       const urls = []
       for (const endpoint of endpoints) {
-        urls.push(axios.get(`https://raw.githubusercontent.com/queryluke/masseffect-5e-data/master/.me5e/${endpoint}.json`))
+        urls.push(axios.get(`https://raw.githubusercontent.com/queryluke/masseffect-5e-data/v130/.me5e/${endpoint}.json`))
       }
       const data = await Promise.all(urls)
       const routes = []
       for (let i = 0; i < data.length; i++) {
         const endpoint = endpoints[i]
-        const path = endpoint === 'gmg'
-          ? 'guide' : endpoint === 'rules'
-            ? 'manual' : endpoint === 'conditions'
-              ? 'appendix/conditions' : endpoint === 'tool-profs'
-                ? 'appendix/tool-profs' : endpoint
-        for (const obj of data[i].data) {
-          routes.push(`/${path}/${obj.id}`)
+        const path = ['conditions', 'tool-profs'].includes(endpoint) ? `appendix/${endpoint}` : endpoint
+        if (['rules', 'gmg'].includes(endpoint)) {
+          const pages = [...new Set(data[i].data.map(j => j.section))]
+          for (const page of pages) {
+            const rPath = endpoint === 'rules' ? 'manual' : 'guide'
+            routes.push(`/${rPath}/${page}`)
+          }
+        } else {
+          for (const obj of data[i].data) {
+            routes.push(`/${path}/${obj.id}`)
+          }
         }
       }
       return routes
