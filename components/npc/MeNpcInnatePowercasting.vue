8d4a8a5a3e18d1521f7f526da66f7d9424d8b40a
Luke Aeschleman <lukeaeschleman@gmail.com>
2021-03-29 21:44:11 -0400

    fix missing innate powerscasting level

--- components/npc/MeNpcInnatePowercasting.vue
+++ components/npc/MeNpcInnatePowercasting.vue
@@ -3,7 +3,7 @@
     p(v-for="(item, index) in powercasting.powerList" v-bind:key="index").my-0 {{ item.perDay }}:
       span(v-for="(power, index) in item.powers" v-bind:key="power.id").ml-1
         span #[em {{ power.id }}]
-        span(v-if="power.higherLevel").ml-1 (as a {{ power.higherLevel | ordinal }}-level power)
+        span(v-if="power.higherLevel").ml-1 (as a {{ power.higherLevel }}{{ power.higherLevel | ordinal }}-level power)
         span(v-if="index + 1 < item.powers.length") ,
 </template>
 
