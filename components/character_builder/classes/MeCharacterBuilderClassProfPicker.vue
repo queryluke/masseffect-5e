bfb64477e318fdc3b13f557700a6f408cdfc05a4
bdc4 <17170670+bdc4@users.noreply.github.com>
2021-05-15 15:09:41 -0700

    profPicker updates

--- components/character_builder/classes/MeCharacterBuilderClassProfPicker.vue
+++ components/character_builder/classes/MeCharacterBuilderClassProfPicker.vue
@@ -4,7 +4,7 @@
     <br/>
     <v-row v-for="(profObj, profKey) in profs" :key="profKey">
       <v-col cols="12" v-if="profObj.has || profObj.choices">
-        <h4>{{profKey}}</h4>
+        <h4>{{profTitles[profKey] || profKey}}</h4>
         <div class="d-flex">
           <template v-for="(startingProf,startProfKey) in profObj.has">
             <v-chip
@@ -16,13 +16,15 @@
           </template>
           <v-autocomplete
             v-if="profObj.choices"
-            :items="profObj.choices.items"
-            item-text="name"
-            item-value="type"
+            item-text="text"
+            item-value="value"
+            :items="profsAsItems[profKey]"
             :label="profObj.text"
             :counter="profObj.choices.count"
             chips
             multiple
+            @change="writeProps($event, profObj, profKey)"
+            v-model="profSelectModel[profKey]"
           />
         </div>
       </v-col>
@@ -32,14 +34,36 @@
 
 <script>
 export default {
+  data () {
+    return {
+      profTitles: {
+        armor: 'Armor',
+        tool: 'Tools',
+        weapon: 'Weapons',
+        skill: 'Skills',
+        savingThrow: 'Saving Throws'
+      }
+    }
+  },
   props: {
     classIndex: {
       mandatory: true
     }
   },
   methods: {
-    writeProps () {
-      return false
+    writeProps (selectedProfs, profObj, profKey) {
+      const mandProfs = profObj.has ? profObj.has : []
+      const newProfs = selectedProfs.concat(mandProfs)
+      const newProfObj = {}
+      for (const prof in newProfs) {
+        newProfObj[newProfs[prof]] = {
+          expert: false
+        }
+      }
+      this.profs = {
+        type: profKey,
+        profs: newProfObj
+      }
     }
   },
   computed: {
@@ -50,14 +74,60 @@ export default {
     classes () {
       return this.$store.getters.getData('classes')
     },
+    selectedClassProfs: {
+      get () {
+        return this.character.classes[this.classIndex].profs
+      }
+    },
+    profSelectModel () {
+      const model = {}
+      for (const p in this.selectedClassProfs) {
+        const t = this.selectedClassProfs[p]
+        for (const v in t) {
+          console.log(v, 'ignore me')
+          model[p] = []
+          for (const key in Object.keys(t)) {
+            model[p].push({
+              value: Object.keys(t)[key],
+              text: Object.keys(t)[key].toUpperCase()
+            })
+          }
+        }
+      }
+      return model
+    },
     startingClass () {
       return this.character.classes[this.classIndex]
     },
     startingClassData () {
       return this.startingClass && this.classes.find(({ id }) => id === this.startingClass.id)
     },
-    profs () {
-      return this.startingClassData.profs
+    profs: {
+      get () {
+        return this.startingClassData.profs
+      },
+      set (value) {
+        return this.$store.commit('cb/UPDATE_CHARACTER', {
+          cid: this.$route.query.cid,
+          attr: 'classes.' + this.classIndex.toString() + '.profs.' + value.type.toString(),
+          value: value.profs
+        })
+      }
+    },
+    profsAsItems () {
+      const o = {}
+      for (const prof in this.profs) {
+        o[prof] = []
+        if (this.profs[prof].choices && this.profs[prof].choices.items) {
+          this.profs[prof].choices.items.forEach(function (c) {
+            o[prof].push({
+              text: c.toUpperCase(),
+              value: c
+            })
+          })
+        }
+      }
+      return o
     }
   }
 }
