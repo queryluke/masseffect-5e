03f0ae0a790b19472cb7403fb50d201da4227d5e
Luke Aeschleman <lukeaeschleman@gmail.com>
2022-02-09 08:33:45 -0500

    fix character import

--- components/cs/MeCsNis.vue
+++ components/cs/MeCsNis.vue
@@ -116,9 +116,9 @@ export default {
       const blob = new Blob([data], { type: 'text/plain' })
       const e = document.createEvent('MouseEvents')
       const a = document.createElement('a')
-      const filename = this.character.name.replace(/[^a-z0-9]/gi, '')// .toLowerCase()
+      const filename = (this.character.name || 'Unnamed').replace(/[^a-z0-9]/gi, '')// .toLowerCase()
 
-      a.download = `${filename}-${this.cid}.json`
+      a.download = `${filename}-${this.id}.json`
       a.href = window.URL.createObjectURL(blob)
       a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
       e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
