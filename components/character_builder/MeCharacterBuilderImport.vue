c4bd10845a78403217337f6c6c86ca9c3d0ea1df
Luke Aeschleman <lukeaeschleman@gmail.com>
2022-02-09 07:58:43 -0500

    add support for multicharacter exports/imports

--- components/character_builder/MeCharacterBuilderImport.vue
+++ components/character_builder/MeCharacterBuilderImport.vue
@@ -1,17 +1,37 @@
 <template>
-  <div>
+  <div class="d-flex">
     <input ref="file" type="file" style="display: none" @load="character = $event" @change="loadTextFromFile">
-    <v-btn color="primary" @click="$refs.file.click()">
-      <v-icon left>
-        mdi-upload
-      </v-icon>
-      Import
+    <v-menu offset-y>
+      <template #activator="{ on, attrs }">
+        <v-btn color="primary" v-bind="attrs" v-on="on">
+          Import
+          <v-icon right>
+            mdi-menu-down
+          </v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item @click="multiple = false; $refs.file.click()">
+          <v-list-item-title>Single Character</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="multiple = true; $refs.file.click()">
+          <v-list-item-title>Multiple (in a single file)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <v-btn @click="saveAllCharacters" class="ml-2">
+      Export All
     </v-btn>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      multiple: false
+    }
+  },
   methods: {
     loadTextFromFile (ev) {
       const file = ev.target.files[0]
@@ -24,9 +44,29 @@ export default {
         const dataDump = e.target.result
         if (dataDump && dataDump !== '') {
           const char = JSON.parse(e.target.result)
-          this.$store.commit('cb/UPDATE_CHARACTERS', char)
+          if (this.multiple) {
+            for (const character of Object.values(char)) {
+              this.$store.commit('cb/UPDATE_CHARACTERS', character)
+            }
+          } else {
+            this.$store.commit('cb/UPDATE_CHARACTERS', char)
+          }
         }
       }
+    },
+    saveAllCharacters () {
+      const characters = this.$store.getters['cb/characters']
+      const data = JSON.stringify(characters)
+      const blob = new Blob([data], { type: 'text/plain' })
+      const e = document.createEvent('MouseEvents')
+      const a = document.createElement('a')
+
+      const filename = Object.keys(characters).join('-')
+      a.download = `ME5e_Characters-${filename}.json`
+      a.href = window.URL.createObjectURL(blob)
+      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+      e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+      a.dispatchEvent(e)
     }
   }
 
