f1972c355d64276c08ea6f9cb4772bbdd3914f45
bdc4 <17170670+bdc4@users.noreply.github.com>
2022-02-16 17:21:41 -0800

    clear logs logic

--- components/cs/MeCsLogs.vue
+++ components/cs/MeCsLogs.vue
@@ -4,29 +4,32 @@
       Logs
     </me-cs-card-title>
     <div class="mt-3">
-      <!--pre>{{JSON.stringify(logs, undefined, 4)}}</pre-->
-      <v-list v-if="logs.length">
-        <v-list-item v-for="(entry, index) in logs" :key="index" class="pb-2">
-          <v-card v-if="entry.type == 'dice-roll' ">
-            <v-card-title class="pb-2">{{entry.title || 'Title Here'}}</v-card-title>
-            <v-card-subtitle >{{entry.subtitle}}</v-card-subtitle>
-            <v-card-text>
-              {{entry.data}}
-                <!-- TODO: would be nice to have a fun UI... -->
-                <!--template v-for="(roll, rollIndex) in entry.rolls">
-                  <v-col :key="index + ':' + rollIndex" class="text-center">
-                    <v-img
-                    :src="require('~/assets/images/misc/d20blank.png')"
-                    max-width="60"/>
-                    <div class="">
-                      <span>{{roll.value}}</span>
-                    </div>
-                  </v-col>
-                </template-->
-            </v-card-text>
-          </v-card>
-        </v-list-item>
-      </v-list>
+      <template v-if="logs.length">
+        <!--pre>{{JSON.stringify(logs, undefined, 4)}}</pre-->
+        <v-list>
+          <v-list-item v-for="(entry, index) in logs" :key="index" class="pb-2">
+            <v-card v-if="entry.type == 'dice-roll' ">
+              <v-card-title class="pb-2">{{entry.title || 'Title Here'}}</v-card-title>
+              <v-card-subtitle >{{entry.subtitle}}</v-card-subtitle>
+              <v-card-text>
+                {{entry.data}}
+                  <!-- TODO: would be nice to have a fun UI... -->
+                  <!--template v-for="(roll, rollIndex) in entry.rolls">
+                    <v-col :key="index + ':' + rollIndex" class="text-center">
+                      <v-img
+                      :src="require('~/assets/images/misc/d20blank.png')"
+                      max-width="60"/>
+                      <div class="">
+                        <span>{{roll.value}}</span>
+                      </div>
+                    </v-col>
+                  </template-->
+              </v-card-text>
+            </v-card>
+          </v-list-item>
+        </v-list>
+        <v-btn @click="clearLogs()">Clear Logs</v-btn>
+      </template>
       <v-banner v-if="!logs.length">No Logs Found</v-banner>
     </div>
   </div>
@@ -38,6 +41,11 @@ export default {
   computed: {
     logs () {
       return this.$store.getters['character/logs'] || []
+    }
+  },
+  methods: {
+    clearLogs () {
+      return this.$store.dispatch('character/LOG_DESTROY')
     }
   }
 }
