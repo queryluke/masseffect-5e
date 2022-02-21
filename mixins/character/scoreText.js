4637bbe05fa5fa4ce6a2019bb1f960a86e3e3d31
Luke Aeschleman <lukeaeschleman@gmail.com>
2022-02-20 21:52:50 -0500

    fix negative mod dispaly

--- mixins/character/scoreText.js
+++ mixins/character/scoreText.js
@@ -2,6 +2,9 @@ export const ScoreText = {
   methods: {
     modText (score) {
       return score > 0 ? `+${score}` : score
+    },
+    damageText (score) {
+      return score > 0 ? ` + ${score}` : ` - ${score * -1}`
     }
   }
 }
